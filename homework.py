e7ff1781713e1e01451859c35df8738c5bbd3070
Кирюшин Виталий <kiryuschin.vitalij@yandex.ru>
2023-05-29 03:09:55 +0300

    Add 'send_message' and 'check_tokens'

--- homework.py
+++ homework.py
@@ -1,11 +1,19 @@
-...
+import logging
+import os
+import time
+from sys import stdout
+
+import requests
+from dotenv import load_dotenv
+from telegram import Bot, ReplyKeyboardMarkup, TelegramError
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN: str = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN: str = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,24 +26,96 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_FILE = os.path.join(BASE_DIR, 'homework.log')
+LOG_FORMAT = '%(asctime)s [%(levelname)s] %(message)s %(name)s'
 
-def check_tokens():
-    ...
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename=LOG_FILE,
+    filemode='w',
+    format=LOG_FORMAT,
+)
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(stream=stdout))
+
+
+def check_tokens() -> bool:
+    """Проверяет доступность переменных окружения.
+
+    Returns:
+        bool: Доступность переменных окружения
+    """
+    env_vars: set = (
+        'PRACTICUM_TOKEN',
+        'TELEGRAM_TOKEN',
+        'TELEGRAM_CHAT_ID',
+    )
+    flag: bool = True
+    for var in env_vars:
+        if var not in os.environ:
+            logger.critical(
+                'Отсутствует обязательная переменная окружения: '
+                f'"{var}" '
+                'Программа принудительно остановлена.'
+            )
+            flag = False
+    return flag
 
 
 def send_message(bot, message):
-    ...
+    """Отправляет сообщение в Telegram чат.
+
+    Args:
+        bot (Bot): экземпляр класса Bot
+        message (str): сообщение
+    """
+    try:
+        send_message_params = {
+            'chat_id': TELEGRAM_CHAT_ID,
+            'text': message,
+        }
+        bot.send_message(**send_message_params)
+        logger.info(
+            f'Сообщение успешно отправлено: {message}'
+        )
+    except TelegramError as error:
+        logger.error(
+            f'Сообщение не отправлено: {error}'
+        )
+    except Exception:
+        logger.error(
+            f'Непредвиденная ошибка {Exception}'
+        )
 
 
 def get_api_answer(timestamp):
+    """Делает запрос к единственному эндпоинту API-сервиса.
+
+    Args:
+        timestamp (int): временная метка
+    """
     ...
 
 
 def check_response(response):
+    """Проверяет ответ API на соответствие документации.
+
+    Args:
+        response (Response): ответ API
+    """
     ...
 
 
 def parse_status(homework):
+    """Извлекает из информации о конкретной домашней работе статус этой работы.
+
+    Args:
+        homework (_type_): _description_
+
+    Returns:
+        str: вердикт
+    """
     ...
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
@@ -43,13 +123,16 @@ def parse_status(homework):
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot = Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
+    payload = {'from_date': timestamp}
 
-    ...
+    requests_params = {
+        'url': ENDPOINT,
+        'headers': HEADERS,
+        'params': payload,
+    }
+    response = requests.get(**requests_params)
 
     while True:
         try:
@@ -63,4 +146,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    print(check_tokens())
